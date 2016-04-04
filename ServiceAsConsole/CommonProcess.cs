aa8b4570724fa3b0ec6914e4611d4f8bca18330b
ExNought <github@thinkexo.de>
2016-04-04 22:46:23 +0200

    Started commenting existing methods and properties

--- ServiceAsConsole/CommonProcess.cs
+++ ServiceAsConsole/CommonProcess.cs
@@ -6,17 +6,31 @@ namespace ServiceAsConsole
     {
         private string path;
         private string parameters;
+        private bool validatePath;
 
+        /// <summary>
+        /// This contains the path to the executable.
+        /// If ValidatePath is true the Property checks if the 
+        /// </summary>
         public string Path
         {
             get { return path; }
             set { path = value; }
         }
         
+        /// <summary>
+        /// 
+        /// </summary>
         public string Parameters
         {
             get { return parameters; }
             set { parameters = value; }
+        }
+
+        public bool ValidatePath
+        {
+            get { return validatePath; }
+            set { validatePath = value; }
         }
         
         internal Returncodes.Returncode Start()
